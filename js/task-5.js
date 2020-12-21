f1e9852f05b13506ac361b164663835f642749d5
ragimovalik <ragimovalik@gmail.com>
2020-12-21 19:14:31 +0200

    Рефакторинг задачи - 5

--- js/task-5.js
+++ js/task-5.js
@@ -11,6 +11,8 @@
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
-  inputEl.addEventListener("input", (event) => {
-    outputEl.textContent = event.target.value || "незнакомец";
-  });
+const inputedNameHandler = (event) => {
+  outputEl.textContent = event.target.value || "незнакомец";
+};
+
+inputEl.addEventListener("input", inputedNameHandler);
